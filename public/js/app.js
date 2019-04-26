2ecd1fb23082809d1c287497fa461a0167407e22
S3K Developers <sarat@s3kdevelopers.com>
2019-04-26 16:45:38 +0100

    Latest Updates with Upcoming Events

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 
 // Theme
-var theme = 'auto';
+var theme = 'ios';
 if (document.location.search.indexOf('theme=') >= 0) {
   theme = document.location.search.split('theme=')[1].split('&')[0];
 }
